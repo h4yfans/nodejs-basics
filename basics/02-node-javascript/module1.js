8f74da683ce6a125a4c2638107fcef90454db118
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-03 20:31:25 +0300

    node-js basics

---
+++ basics/02-node-javascript/module1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Kaan on 10/3/2016.
+ */
+
+function myFunction() {
+    console.log('Function was called!');
+}
+
+var myString = 'String!';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ basics/02-node-javascript/module2.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Kaan on 10/3/2016.
+ */
+module.exports = {
+    myFunction: function () {
+        console.log('exported');
+    },
+    myVariable: 'Exported Variable'
+};
